94a93f086ad4a87989c406c84e1e9a0bf6f82926
ljq <luojq@citydo.com.cn>
2021-07-16 15:08:15 +0800

    mocha 钩子

--- tests/unit/indexDB.spec.js
+++ tests/unit/indexDB.spec.js
@@ -1,35 +1,39 @@
 const IndexDB  = require('../../src/utils/indexDB.js')
 // import IndexDB from '../../src/utils/indexDB'
 
-// 需要先清空数据库
 describe('indexDB', ()=>{
     const testDB = new IndexDB('testDatabase','testStore')
+    before(async ()=>{
+        await testDB.open()
+        await testDB.add({id: 1,name: 'test1'})
+        
+    })
 
     it('open', async ()=>{
-        await testDB.open()
         expect(testDB.db).to.be.an.instanceOf(IDBDatabase)
     })
 
-    it('add and query',async ()=>{
-        await testDB.open()
-        await testDB.add({name: 'test1'})
+    it('query', async ()=>{
         const result = await testDB.queryById(1)
         expect(result).to.deep.equal({id: 1,name: 'test1'})
     })
 
+    it('add and query',async ()=>{
+        await testDB.add({id: 2,name: 'test2'})
+        const result = await testDB.queryById(2)
+        expect(result).to.deep.equal({id: 2,name: 'test2'})
+        
+    })
+
     it('query page size', async ()=>{
-        await testDB.open()
-        await testDB.add({name: 'test1'})
         const result = await testDB.queryList(1, 1)
         expect(result.length).to.equal(1)
-        await testDB.add({name: 'test2'})
         const result2 = await testDB.queryList(1, 2)
         expect(result2.length).to.equal(2)
     })
 
 
     it('update and query', async ()=>{
-        await testDB.open()
         const result = await testDB.queryById(1)
         expect(result).to.deep.equal({id: 1,name: 'test1'})
         await testDB.update({id: 1, name: 'test-1'})
@@ -38,12 +42,16 @@ describe('indexDB', ()=>{
     })
 
     it('delete and query', async ()=>{
-        await testDB.open()
-        const result = await testDB.queryById(1)
-        expect(result).to.deep.equal({id: 1,name: 'test-1'})
-        await testDB.deleteById(1)
-        const result2 = await testDB.queryById(1)
-        expect(result2).to.equal(undefined)
+        const result = await testDB.queryById(2)
+        expect(result).to.deep.equal({id: 2,name: 'test2'})
+        await testDB.deleteById(2)
+        const result2 = await testDB.queryById(2)
+        expect(result2).to.be.undefined
 
+    })
+
+    after(async ()=>{
+        await testDB.deleteById(1)
+        await testDB.deleteById(2)
     })
 })
